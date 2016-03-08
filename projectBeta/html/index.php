86988c23e490cff9955da9538b4a8ae7f1bb483a
ykn121 <ykn121@gmail.com>
2016-03-08 22:12:56 +0800

    edit by matthew

---
+++ projectBeta/html/index.php
@@ -0,0 +1,96 @@
+﻿<?php
+	include("header.php");
+	
+?>
+
+<body>
+<div id="layout">
+  <div id="header">
+    <h1 id="logo"><a href="http://all-free-download.com/free-website-templates/">SimpleEvent</a></h1>
+    <span id="slogan">Your slogan goes here</span>
+    <hr class="noscreen" />
+    <p class="noscreen noprint"> <em>Rychlá navigace: <a href="http://all-free-download.com/free-website-templates/">obsah</a>, <a href="http://all-free-download.com/free-website-templates/">navigace</a>.</em></p>
+    <div id="quicknav"> <a href="http://all-free-download.com/free-website-templates/">Home</a> <a href="http://all-free-download.com/free-website-templates/">Contact</a> <a href="http://all-free-download.com/free-website-templates/">Sitemap</a> </div>
+    <div id="search">
+      <form href="http://all-free-download.com/free-website-templates/" method="post">
+        <fieldset>
+        <input type="text" id="phrase" name="phrase" value="search phrase" onfocus="if(this.value=='search phrase')this.value=''" />
+        <input type="submit" id="submit" value="SEARCH" />
+        </fieldset>
+      </form>
+    </div>
+  </div>
+  <hr class="noscreen" />
+  <div id="nav" class="box">
+    <ul>
+      <li id="active"><a href="index.php">Home</a></li>
+      <li><a href="driver.php">I am a Driver</a></li>
+      <li><a href="passenger.php">I am a Passenger</a></li>
+      <!--li><a href="http://all-free-download.com/free-website-templates/">Portfolio</a></li>
+      <li><a href="http://all-free-download.com/free-website-templates/" class="nosep">Contacts</a></li>-->
+    </ul>
+    <hr class="noscreen" />
+  </div>
+  <div id="container" class="box">
+    <div id="obsah" class="content box">
+      <div class="in">
+        <div id="new-article">
+          <div id="corner">
+            <h2><a href="http://all-free-download.com/free-website-templates/">Lorem ipsum</a></h2>
+            <div class="f-left article-img"><img src="tmp/image.jpg" alt="" />
+              <div></div>
+            </div>
+            <p class="f-left"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="http://all-free-download.com/free-website-templates/" class="more">MORE</a></p>
+            <div class="clear"></div>
+          </div>
+        </div>
+        <div class="article">
+          <h2><a href="http://all-free-download.com/free-website-templates/">Lorem ipsum</a></h2>
+          <div class="f-left article-img"><img src="tmp/image.jpg" alt="" />
+            <div></div>
+          </div>
+          <p class="f-left"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="http://all-free-download.com/free-website-templates/" class="more">MORE</a></p>
+          <div class="clear"></div>
+        </div>
+        <div class="article">
+          <h2><a href="http://all-free-download.com/free-website-templates/">Lorem ipsum</a></h2>
+          <div class="f-left article-img"><img src="tmp/image.jpg" alt="" />
+            <div></div>
+          </div>
+          <p class="f-left"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="http://all-free-download.com/free-website-templates/" class="more">MORE</a></p>
+          <div class="clear"></div>
+        </div>
+        <div class="article">
+          <h2><a href="http://all-free-download.com/free-website-templates/">Lorem ipsum</a></h2>
+          <div class="f-left article-img"><img src="tmp/image.jpg" alt="" />
+            <div></div>
+          </div>
+          <p class="f-left"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="http://all-free-download.com/free-website-templates/" class="more">MORE</a></p>
+          <div class="clear"></div>
+        </div>
+        <div class="article">
+          <h2><a href="http://all-free-download.com/free-website-templates/">Lorem ipsum</a></h2>
+          <div class="f-left article-img"><img src="tmp/image.jpg" alt="" />
+            <div></div>
+          </div>
+          <p class="f-left"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="http://all-free-download.com/free-website-templates/" class="more">MORE</a></p>
+          <div class="clear"></div>
+        </div>
+        <a href="http://all-free-download.com/free-website-templates/" class="older">&laquo; Older articles</a> </div>
+    </div>
+    <div id="panel-right" class="box panel">
+		<div id="bottom">
+			<?php include("sidebar.php")?>
+		</div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div id="foot">
+    <div id="page-bottom"> <a href="#header">Go up</a> </div>
+    <p class="f-left">&copy; 2008 - <a href="http://all-free-download.com/free-website-templates/">SimpleEvent</a></p>
+    <p class="f-right"><a href="http://www.tvorimestranky.cz" id="webdesign">Webdesign</a>: <a href="http://www.tvorimestranky.cz">TvorimeStranky.cz</a> | Sponsored by: <a href="http://www.topas-tachlovice.cz/topas-tachlovice.aspx">Tachlovice</a></p>
+  </div>
+</div>
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates</a></div></body>
+</html>
