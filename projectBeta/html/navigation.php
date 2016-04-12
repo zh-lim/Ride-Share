3cf5206b19af6274555d209f70fe0bb714280684
Zi Hui Lim <ZEEee@Zis-MacBook-Pro.local>
2016-04-12 11:11:26 +0800

    edited nav.php

--- projectBeta/html/navigation.php
+++ projectBeta/html/navigation.php
@@ -4,6 +4,7 @@
       <li <?php if (!isset($active1)) $active1=""; echo $active1; ?>><a href="ridePosting.php">Offer Rides</a></li>
       <li <?php if (!isset($active2)) $active2=""; echo $active2; ?>><a href="rideHistory.php">Rides Offered</a></li>
       <li <?php if (!isset($active4)) $active4=""; echo $active4; ?>><a href="passengerRecord.php">Request History</a></li>
+      <li <?php if (!isset($active5)) $active5=""; echo $active5; ?>><a href="search.php">Search Rides</a></li>
     </ul>
     <hr class="noscreen" />
   </div>
